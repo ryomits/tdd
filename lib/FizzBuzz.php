30c0f7ebda18e7559dd5cc406ea53fa4d6c9353a
ryota.mitsui <ryota.mitsui@gaiax.com>
2016-06-01 09:37:20 +0000

    一般化した

--- lib/FizzBuzz.php
+++ lib/FizzBuzz.php
@@ -2,15 +2,27 @@
 class FizzBuzz
 {
 	public static function run($value) {
-		if ($value == 0) {
-			return $value;
-		} elseif ($value % 15 == 0) {
-			return 'FizzBuzz';
-		} elseif ($value % 5 == 0) {
-			return 'Buzz';
-		} elseif ($value % 3 == 0) {
-			return 'Fizz';
+		if (!static::isMultipleOfAll($value)) {
+			if (static::isMultipleOf(15, $value)) {
+				return 'FizzBuzz';
+			}
+
+			if (static::isMultipleOf(5, $value)) {
+				return 'Buzz';
+			}
+
+			if (static::isMultipleOf(3, $value)) {
+				return 'Fizz';
+			}
 		}
-		return "$value";
+		return $value;
+	}
+
+	private static function isMultipleOfAll($value) {
+		return $value == 0;
+	}
+
+	private static function isMultipleOf($multiple, $value) {
+		return $value % $multiple == 0;
 	}
 }
