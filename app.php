037fe3642f01b2bf09d3ad8a3c56964d82cfbfcf
ryota.mitsui <ryota.mitsui@gaiax.com>
2016-06-15 06:53:20 +0000

    Selector利用

--- app.php
+++ app.php
@@ -1,7 +1,28 @@
 <?php
-while (true) {
-	$value = trim(fgets(STDIN));
-	if ($value == 0) {
-		break;
+require('lib/Selector.php');
+
+class StdIn
+{
+	public function in()
+	{
+		return trim(fgets(STDIN));
 	}
 }
+
+class StdOut
+{
+	public function out($param)
+	{
+		print "$param\n";
+	}
+}
+
+while (true) {
+	$value = trim(fgets(STDIN));
+	if ($value === '0') {
+		break;
+	}
+
+	(new Selector(new StdIn, new StdOut))->select($value);
+}
+
