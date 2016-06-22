96e3b1127593570cc82fb8a7002bea7dc957d8a2
ryota.mitsui <ryota.mitsui@gaiax.com>
2016-06-22 09:57:36 +0000

    tearDown追加

--- test/FileOutTest.php
+++ test/FileOutTest.php
@@ -2,15 +2,20 @@
 require_once('FileOut.php');
 class FileOutTest extends PHPUnit_Framework_TestCase
 {
+	protected function tearDown()
+	{
+		unlink('/tmp/data.txt');
+	}
+
 	/**
 	 * @test
 	 */
 	public function testログを永続化できること() {
 		$file = new FileOut();
-		$file->set(["3: Fizz", "5: Buzz"]);
+		$file->set(["3: Fizz", "5: Buzz", "15: FizzBuzz"]);
 
 		$rows = explode("\n", file_get_contents('/tmp/data.txt'));
-		$this->assertEquals($rows, ["3: Fizz", "5: Buzz"]);
+		$this->assertEquals($rows, ["3: Fizz", "5: Buzz", "15: FizzBuzz"]);
 	}
 
 	/**
