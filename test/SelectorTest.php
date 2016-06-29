2f4be8ca1f1a77aec216ef153b7304077d003edb
ryota.mitsui <ryota.mitsui@gaiax.com>
2016-06-29 07:38:49 +0000

    共通の処理切り出し

--- test/SelectorTest.php
+++ test/SelectorTest.php
@@ -2,44 +2,44 @@
 require_once('Selector.php');
 class SelectorTest extends PHPUnit_Framework_TestCase
 {
+	protected function setUp()
+	{
+		$this->selector = new Selector(null, null, null, null);
+	}
+
 	/**
 	 * @test
 	 */
 	public function test1が渡るとFizzBuzzを実行すること() {
-		$selector = new Selector(null, null, null, null);
-		$this->assertTrue($selector->select('1') instanceof FizzBuzzExec);
+		$this->assertTrue($this->selector->select('1') instanceof FizzBuzzExec);
 	}
 
 	/**
 	 * @test
 	 */
 	public function test2が渡るとFizzBuzzLogShowインスタンスを返すこと() {
-		$selector = new Selector(null, null, null, null);
-		$this->assertTrue($selector->select('2') instanceof FizzBuzzLogShow);
+		$this->assertTrue($this->selector->select('2') instanceof FizzBuzzLogShow);
 	}
 
 	/**
 	 * @test
 	 */
 	public function test3が渡るとFizzBuzzLoggingインスタンスを返すこと() {
-		$selector = new Selector(null, null, null, null);
-		$this->assertTrue($selector->select('3') instanceof FizzBuzzLogging);
+		$this->assertTrue($this->selector->select('3') instanceof FizzBuzzLogging);
 	}
 
 	/**
 	 * @test
 	 */
 	public function test4が渡るとFizzBuzzPermanentLogShowインスタンスを返すこと() {
-		$selector = new Selector(null, null, null, null);
-		$this->assertTrue($selector->select('4') instanceof FizzBuzzPermanentLogShow);
+		$this->assertTrue($this->selector->select('4') instanceof FizzBuzzPermanentLogShow);
 	}
 
 	/**
 	 * @test
 	 */
 	public function testメニューにない値が渡るとFizzBuzzNotDoingインスタンスを返すこと() {
-		$selector = new Selector(null, null, null, null);
 		$illegalValue = '100';
-		$this->assertTrue($selector->select($illegalValue) instanceof FizzBuzzNotDoing);
+		$this->assertTrue($this->selector->select($illegalValue) instanceof FizzBuzzNotDoing);
 	}
 }
