71e3631bfa2a5b07dd98c2d5c7738b2739fa9d5b
ryota.mitsui <ryota.mitsui@gaiax.com>
2016-06-29 07:28:24 +0000

    ログ保存のテストリファクタ

---
+++ test/FizzBuzzLoggingTest.php
@@ -0,0 +1,18 @@
+<?php
+require_once('FizzBuzzLogging.php');
+require_once('StdOutSpy.php');
+require_once('Logger.php');
+class FizzBuzzLoggingTest extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * @test
+	 */
+	public function test履歴を保存すること() {
+		$spy = new FileSpy();
+		$logger = new Logger();
+		$logger->add('3: Fizz');
+		$logger->add('5: Buzz');
+		(new FizzBuzzLogging($spy, $logger))->run();
+		$this->assertEquals($spy->results(), ["3: Fizz", "5: Buzz"]);
+	}
+}
--- test/SelectorTest.php
+++ test/SelectorTest.php
@@ -26,26 +26,20 @@ class SelectorTest extends PHPUnit_Framework_TestCase
 	/**
 	 * @test
 	 */
+	public function test3が渡るとFizzBuzzLoggingインスタンスを返すこと() {
+		$selector = new Selector(null, null, null, null);
+		$this->assertTrue($selector->select('3') instanceof FizzBuzzLogging);
+	}
+
+	/**
+	 * @test
+	 */
 	public function testメニューにない値が渡るとなにもしないこと() {
 		$spy = new StdOutSpy();
 		$selector = new Selector(null, $spy, null, null);
 		$illegalValue = '100';
 		$selector->select($illegalValue)->run();
 		$this->assertEquals($spy->result(), []);
-	}
-
-	/**
-	 * @test
-	 */
-	public function test3が渡ると履歴が保存されること() {
-		$spy = new FileSpy();
-		$logger = new Logger();
-		$logger->add('3: Fizz');
-		$logger->add('5: Buzz');
-		$selector = new Selector(null, null, $logger, $spy);
-
-		$selector->select('3')->run();
-		$this->assertEquals($spy->results(), ["3: Fizz", "5: Buzz"]);
 	}
 
 	/**
