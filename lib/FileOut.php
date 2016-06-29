f0d71d5627d732300a16f5538f8aff2149f0e49a
ryota.mitsui <ryota.mitsui@gaiax.com>
2016-06-29 05:17:33 +0000

    FileOutリファクタ

--- lib/FileOut.php
+++ lib/FileOut.php
@@ -2,15 +2,23 @@
 
 class FileOut
 {
+	public function __construct($filepath)
+	{
+		$this->filepath = $filepath;
+	}
+
 	public function set($results)
 	{
-		$file = fopen('/tmp/data.txt', 'w');
+		$file = fopen($this->filepath, 'w');
 		fwrite($file, implode("\n", $results));
 		fclose($file);
 	}
 
 	public function get()
 	{
-		return explode("\n", file_get_contents('/tmp/data.txt'));
+		if (!file_exists($this->filepath)) {
+			return [];
+		}
+		return explode("\n", file_get_contents($this->filepath));
 	}
 }
--- test/FileOutTest.php
+++ test/FileOutTest.php
@@ -2,29 +2,39 @@
 require_once('FileOut.php');
 class FileOutTest extends PHPUnit_Framework_TestCase
 {
+	private $filepath = '/tmp/data.txt';
+
+	protected function setUp()
+	{
+		$this->delete_file();
+		$this->file = new FileOut($this->filepath);
+	}
+
 	protected function tearDown()
 	{
-		unlink('/tmp/data.txt');
+		$this->delete_file();
+	}
+
+	private function delete_file()
+	{
+		if (file_exists($this->filepath)) {
+			unlink($this->filepath);
+		}
 	}
 
 	/**
 	 * @test
 	 */
 	public function testログを永続化できること() {
-		$file = new FileOut();
-		$file->set(["3: Fizz", "5: Buzz", "15: FizzBuzz"]);
+		$this->file->set(["3: Fizz", "5: Buzz", "15: FizzBuzz"]);
 
-		$rows = explode("\n", file_get_contents('/tmp/data.txt'));
-		$this->assertEquals($rows, ["3: Fizz", "5: Buzz", "15: FizzBuzz"]);
+		$this->assertEquals($this->file->get(), ["3: Fizz", "5: Buzz", "15: FizzBuzz"]);
 	}
 
 	/**
 	 * @test
 	 */
-	public function test永続化した情報がかえること() {
-		$file = new FileOut();
-		$file->set(["3: Fizz", "5: Buzz"]);
-
-		$this->assertEquals($file->get(), ["3: Fizz", "5: Buzz"]);
+	public function test永続化した情報がない場合は空の配列が返ること() {
+		$this->assertEquals($this->file->get(), []);
 	}
 }
