ca3321b5aee17539d064f15fa913a7c522f08d71
ryota.mitsui <ryota.mitsui@gaiax.com>
2016-06-29 05:58:18 +0000

    nullオブジェクト追加

--- lib/Selector.php
+++ lib/Selector.php
@@ -3,6 +3,7 @@ require_once('FizzBuzzExec.php');
 require_once('FizzBuzzLogShow.php');
 require_once('FizzBuzzLogging.php');
 require_once('FizzBuzzPermanentLogShow.php');
+require_once('FizzBuzzNotDoing.php');
 class Selector
 {
 	public function __construct($stdIn, $stdOut, $logger, $file) {
@@ -13,14 +14,21 @@ class Selector
 	}
 
 	public function select($value) {
+		$this->select_command($value)->run();
+	}
+
+	private function select_command($value)
+	{
 		if ($value === '1') {
-			(new FizzBuzzExec($this->stdIn, $this->stdOut, $this->logger))->run();
+			return (new FizzBuzzExec($this->stdIn, $this->stdOut, $this->logger));
 		} elseif ($value === '2') {
-			(new FizzBuzzLogShow($this->stdOut, $this->logger))->run();
+			return (new FizzBuzzLogShow($this->stdOut, $this->logger));
 		} elseif ($value === '3') {
-			(new FizzBuzzLogging($this->file, $this->logger))->run();
+			return (new FizzBuzzLogging($this->file, $this->logger));
 		} elseif ($value === '4') {
-			(new FizzBuzzPermanentLogShow($this->file, $this->stdOut))->run();
+			return (new FizzBuzzPermanentLogShow($this->file, $this->stdOut));
+		} else {
+			return new FizzBuzzNotDoing;
 		}
 	}
 }
